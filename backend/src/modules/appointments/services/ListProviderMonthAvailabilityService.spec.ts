263149b756c107ff56da1dd3d9fe52218e4f2037
Gabriel Pereira <gabriel9938@gmail.com>
2020-07-10 14:29:12 -0300

    Listando dias disponiveis

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -25,9 +25,52 @@ describe('List Providers', () => {
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
+      date: new Date(2020, 4, 20, 9, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
       date: new Date(2020, 4, 20, 10, 0, 0),
     });
 
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 11, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 13, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 16, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 17, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 9, 0, 0),
+    });
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
       date: new Date(2020, 4, 21, 8, 0, 0),
@@ -41,10 +84,10 @@ describe('List Providers', () => {
 
     expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, avalible: true },
-        { day: 20, avalible: false },
-        { day: 21, avalible: false },
-        { day: 22, avalible: true },
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
       ]),
     );
   });
