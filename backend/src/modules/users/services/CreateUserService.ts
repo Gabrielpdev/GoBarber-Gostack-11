9103d33272f6698e28282d3d961feb5d74c74f87
Gabriel Pereira <gabriel9938@gmail.com>
2020-07-15 14:16:02 -0300

    Invalidando cache

--- backend/src/modules/users/services/CreateUserService.ts
+++ backend/src/modules/users/services/CreateUserService.ts
@@ -5,6 +5,7 @@ import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/Users';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+import ICacheProvier from '@shared/container/providers/CacheProvider/models/ICacheProvier';
 
 interface IRequest {
   name: string;
@@ -20,6 +21,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvier,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -36,6 +40,8 @@ class CreateUserService {
       email,
       password: passwordHashed,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
