183f306f7242f75c5171da104cb85821374f0569
Gabriel Pereira <gabriel9938@gmail.com>
2020-07-17 15:32:53 -0300

    Implementando redefinição

--- frontend/src/pages/ResetPassword/index.tsx
+++ frontend/src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import React, { useRef, useCallback } from 'react';
 import { FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import * as Yup from 'yup';
 
 import { useToast } from '../../hooks/toast';
@@ -13,16 +13,17 @@ import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErros';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
   password_confirmation: string;
-
 }
 
 const ResetPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const history = useHistory();
+  const location = useLocation();
 
   const { addToast } = useToast();
 
@@ -34,12 +35,25 @@ const ResetPassword: React.FC = () => {
           password: Yup.string().required('Senha obrigatória'),
           password_confirmation: Yup.string().oneOf(
             [Yup.ref('password'), undefined],
-            'Confirmação incorreta'
-          )
+            'Confirmação incorreta',
+          ),
         });
 
         await schema.validate(data, {
           abortEarly: false,
+        });
+
+        const { password, password_confirmation } = data;
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
         });
 
         history.push('/');
@@ -55,12 +69,11 @@ const ResetPassword: React.FC = () => {
         addToast({
           type: 'error',
           title: 'Erro ao resetar senha',
-          description:
-            'Ocorreu um erro ao resetar sua senha, tente novamente',
+          description: 'Ocorreu um erro ao resetar sua senha, tente novamente',
         });
       }
     },
-    [ addToast, history],
+    [addToast, history, location.search],
   );
 
   return (
@@ -70,7 +83,7 @@ const ResetPassword: React.FC = () => {
           <img src={logoImg} alt="logo" />
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Resetar senha</h1>
-            
+
             <Input
               name="password"
               icon={FiLock}
@@ -85,7 +98,6 @@ const ResetPassword: React.FC = () => {
               placeholder="Confirmação de Senha"
             />
             <Button type="submit">Alterar senha</Button>
-
           </Form>
         </AnimationContainer>
       </Content>
